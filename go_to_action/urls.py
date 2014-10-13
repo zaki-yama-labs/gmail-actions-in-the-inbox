124e8de15131c6a8a38ca2aefba675ac89ef96f5
zaki-yama <shingoyamazaki00@gmail.com>
2014-10-13 23:11:39 +0900

    urlのルートを変更

--- go_to_action/urls.py
+++ go_to_action/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import patterns, url
 from .views import MovieListView, NewMovieView, MovieDetailView, EmailSenderView
 
 urlpatterns = patterns('',
-	url(r'^movies/$', MovieListView.as_view(), name='movie_list'),
-	url(r'^movies/new/$', NewMovieView.as_view(), name='movie_new'),
-	url(r'^movies/(?P<pk>\d+)/$', MovieDetailView.as_view(), name='movie_detail'),
+	url(r'^new/$', NewMovieView.as_view(), name='movie_new'),
+	url(r'^(?P<pk>\d+)/$', MovieDetailView.as_view(), name='movie_detail'),
+	url(r'^(?P<pk>\d+)/edit/$', NewMovieView.as_view(), name='movie_edit'),
+	url(r'^(?P<pk>\d+)/email/$', EmailSenderView.as_view(), name='send_email'),
+	url(r'^', MovieListView.as_view(), name='movie_list'),
 )
